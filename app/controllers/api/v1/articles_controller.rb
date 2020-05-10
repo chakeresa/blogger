7b63daf5d68a4dd09092d0700c21112c25a5420c
Alexandra Chakeres <alexchakeres@gmail.com>
2020-05-09 20:15:14 -0600

    Add ArticlesController#create

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -6,4 +6,20 @@ class Api::V1::ArticlesController < ApplicationController
   def show
     render json: ArticleSerializer.render(Article.find(params[:id]))
   end
+
+  def create
+    article = Article.new(article_params)
+
+    if article.save
+      render json: ArticleSerializer.render(article), status: :created
+    else
+      render json: {errors: article.errors}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- spec/requests/api/v1/articles_controller_spec.rb
+++ spec/requests/api/v1/articles_controller_spec.rb
@@ -54,4 +54,58 @@ RSpec.describe Api::V1::ArticlesController do
       expect(article).to eq(expected_hash)
     end
   end
+
+  describe "POST #create" do
+    describe 'with valid attributes' do
+      before(:each) do
+        @valid_attrs = {
+          title: Faker::Dessert.flavor,
+          body: Faker::Lorem.paragraph(
+            sentence_count: 3,
+            supplemental: true,
+            random_sentences_to_add: 3
+          )
+        }
+
+        post "/api/v1/articles", params: {article: @valid_attrs}
+      end
+  
+      it "returns 201" do
+        expect(response).to have_http_status(201)
+      end
+  
+      it "outputs data for the new article" do
+        article = JSON.parse(response.body)
+  
+        expect(article.class).to eq(Hash)
+  
+        expect(article["title"]).to eq(@valid_attrs[:title])
+        expect(article["body"]).to eq(@valid_attrs[:body])
+        expect(article["id"]).to eq(Article.last.id)
+      end
+    end
+
+    describe 'with invalid attributes' do
+      before(:each) do
+        @invalid_attrs = {
+          title: Faker::Dessert.flavor
+          # no body given
+        }
+
+        post "/api/v1/articles", params: {article: @invalid_attrs}
+      end
+  
+      it "returns 422" do
+        expect(response).to have_http_status(422)
+      end
+  
+      it "lists errors" do
+        json = JSON.parse(response.body)
+  
+        expect(json.class).to eq(Hash)
+  
+        expect(json["errors"]).to eq({"body" => ["can't be blank"]})
+      end
+    end
+  end
 end
