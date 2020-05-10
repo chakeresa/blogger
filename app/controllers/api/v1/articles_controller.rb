c66e3820549526cd01038bee00cfdcebdf9a4c2c
Alexandra Chakeres <alexchakeres@gmail.com>
2020-05-09 19:39:15 -0600

    Add ArticlesController#show

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::ArticlesController < ApplicationController
   def index
     render json: ArticleSerializer.render(Article.all)
   end
+
+  def show
+    render json: ArticleSerializer.render(Article.find(params[:id]))
+  end
 end
--- spec/requests/api/v1/articles_controller_spec.rb
+++ spec/requests/api/v1/articles_controller_spec.rb
@@ -19,12 +19,39 @@ RSpec.describe Api::V1::ArticlesController do
       expect(articles.class).to eq(Array)
       expect(articles.count).to eq(@count)
 
-      expected_first =  {
+      expected_first = {
         "id" => @first_article.id,
         "title" => @first_article.title,
         "body" => @first_article.body
       }
       expect(articles.first).to eq(expected_first)
+    end
+  end
+
+  describe "GET #show" do
+    before(:each) do
+      @count = 5
+      @first_article = create(:article)
+      @other_articles =  create_list(:article, @count - 1)
+    end
+
+    it "returns 200" do
+      get "/api/v1/articles/#{@first_article.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it "outputs data for a single article" do
+      get "/api/v1/articles/#{@first_article.id}"
+      article = JSON.parse(response.body)
+
+      expect(article.class).to eq(Hash)
+
+      expected_hash = {
+        "id" => @first_article.id,
+        "title" => @first_article.title,
+        "body" => @first_article.body
+      }
+      expect(article).to eq(expected_hash)
     end
   end
 end
