d7135eb36ebec08d9362932f70f5c55794c32706
Alexandra Chakeres <alexchakeres@gmail.com>
2020-05-10 09:42:55 -0600

    Make serializers simpler

--- app/serializers/article_serializer.rb
+++ app/serializers/article_serializer.rb
@@ -1,8 +1,9 @@
-class ArticleSerializer < BaseSerializer
-  MODEL_CLASS = Article
-  ATTRIBUTES = [
-    :body,
-    :id,
-    :title
-  ]
+class ArticleSerializer
+  def self.render(article_or_articles)
+    article_or_articles.as_json(only: [
+      :body,
+      :id,
+      :title
+    ])
+  end
 end
--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,9 +1,10 @@
-class CommentSerializer < BaseSerializer
-  MODEL_CLASS = Comment
-  ATTRIBUTES = [
-    :article_id,
-    :author_name,
-    :body,
-    :id,
-  ]
+class CommentSerializer
+  def self.render(comment_or_comments)
+    comment_or_comments.as_json(only: [
+      :article_id,
+      :author_name,
+      :body,
+      :id,
+    ])
+  end
 end
