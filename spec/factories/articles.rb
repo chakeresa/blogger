7998eb068eed0b0017ca5883ecde9d2ca229cba6
Alexandra Chakeres <alexchakeres@gmail.com>
2020-05-09 18:55:18 -0600

    Add faker & configure factorybot

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    body { "MyText" }
+    title { Faker::Space.agency }
+    body {
+      Faker::Hipster.paragraph(
+        sentence_count: 2,
+        supplemental: true,
+        random_sentences_to_add: 4
+      )
+    }
   end
 end
